11e72e34d87536827430fd188fa7ab7545c94342
avb15214yp <avb152140@gmail.com>
2022-01-18 21:24:32 +0300

    Поправил USER, добавил app titles

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+ROL_USER = 'user'
+ROL_MODERATOR = 'moderator'
+ROL_ADMIN = 'admin'
+
+ROLES = [
+    (ROL_USER, 'Пользователь'),
+    (ROL_MODERATOR, 'Модератор'),
+    (ROL_ADMIN, 'Администратор')
+]
+
 
 class User(AbstractUser):
-    ROLES = [
-        'user',
-        'moderator',
-        'admin',
-    ]
-    
-    role = models.TextField(
-        ROLES,
-        blank = True,
+
+    role = models.CharField(
+        choices=ROLES,
+        blank=False,
+        null=False,
+        max_length=10
     )
+    # role = models.TextField(
+    #     ROLES,
+    #     blank = True,
+    # )
